edd467a91b0baad94f34701a18317aae5f82df43
Bruce Nielson <brucenielson2@gmail.com>
2017-12-19 10:15:30 -0700

    Fooling around. Got IsIn to work, but way too slow still.

--- IsInTest.py
+++ IsInTest.py
@@ -10,12 +10,14 @@ test_elements = [
                 ]
 
 output = np.isin(element, test_elements)
-print(output)
+#print(output)
 
 
 def multidim_intersect(arr1, arr2):
+    print([('',arr1.dtype)]*arr1.shape[1])
     arr1_view = arr1.view([('',arr1.dtype)]*arr1.shape[1])
     print(arr1_view)
+
     arr2_view = arr2.view([('',arr2.dtype)]*arr2.shape[1])
     intersected = np.intersect1d(arr1_view, arr2_view)
     return intersected.view(arr1.dtype).reshape(-1, arr1.shape[1])
@@ -31,4 +33,5 @@ test_arr2 = np.array([[1, 2],
                          [3, 1],
                          [1, 3]])
 
-print(multidim_intersect(test_arr1, test_arr2))
+output = multidim_intersect(test_arr1, test_arr2)
+print(output)
